1d47912a2e3481c05bf882a3d52de88dd52dcd06
bankass79 <a.guindo@LGOP1378>
2019-06-14 23:52:27 +0200

    pom.xml add dependencies

--- src/fr/sendmail/bankass/busObj/Email.java
+++ src/fr/sendmail/bankass/busObj/Email.java
@@ -1,5 +1,59 @@
 package fr.sendmail.bankass.busObj;
 
-public class Email {
+import java.io.Serializable;
+
+public class Email implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	private String idRegion;
+	private String corps;
+	
+	
+	public Email() {
+		super();
+	}
+
+
+	public Email(long id, String idRegion, String corps) {
+		super();
+		this.id = id;
+		this.idRegion = idRegion;
+		this.corps = corps;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getIdRegion() {
+		return idRegion;
+	}
+
+
+	public void setIdRegion(String idRegion) {
+		this.idRegion = idRegion;
+	}
+
+
+	public String getCorps() {
+		return corps;
+	}
+
+
+	public void setCorps(String corps) {
+		this.corps = corps;
+	}
 
 }
